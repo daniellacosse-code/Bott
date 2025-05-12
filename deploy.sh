621c0679dabf6f57ed3ca22ef8e7c96fd9d58cda
Daniel LaCosse <3759828+daniellacosse@users.noreply.github.com>
2025-05-12 10:01:15 -0400

    update deploy variables

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,11 @@ gcloud run deploy bott-service \
   --cpu 1 \
   --cpu-boost \
   --memory 2Gi \
-  --min-instances 1 \
+  --min-instances 0 \
   --max-instances 1 \
   --set-env-vars DISCORD_TOKEN=$DISCORD_TOKEN \
   --set-env-vars GOOGLE_PROJECT_ID=$GOOGLE_PROJECT_ID \
   --set-env-vars GOOGLE_PROJECT_LOCATION=$GOOGLE_PROJECT_LOCATION \
-  --set-env-vars CONFIG_PROACTIVE_REPLY_CHANCE=$CONFIG_PROACTIVE_REPLY_CHANCE \
-  --set-env-vars CONFIG_HISTORY_LENGTH=$CONFIG_HISTORY_LENGTH \
   --set-env-vars CONFIG_RATE_LIMIT_VIDEOS=$CONFIG_RATE_LIMIT_VIDEOS \
   --set-env-vars CONFIG_RATE_LIMIT_IMAGES=$CONFIG_RATE_LIMIT_IMAGES \
   --allow-unauthenticated
